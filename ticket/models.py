a9bd2fb3fca943eb1d2c556ed3f7c778e2e14071
Акакиев Акакий <BlackWiind@gmail.com>
2023-04-06 10:33:39 +1000

    Initial commit

---
+++ ticket/models.py
@@ -0,0 +1,11 @@
+import uuid
+from django.db import models
+from users.models import User
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+    date_created = models.DateTimeField(auto_now_add=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
---
+++ users/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .form import RegisterCostumerForm
+
+
+def register_custumer(request):
+    if request.method == 'POST':
+        form = RegisterCostumerForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_customer = True
+            user.save()
+            messages.info(request, 'Пользователь успешно зарегестрирован.')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Что-то пошло не так. Проверьте введённую информацию.')
+            return redirect('register-customer')
+    else:
+        form = RegisterCostumerForm()
+        context = {'form':form}
+        return render(request, 'user/register_customer.html', context)
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            messages.info(request, 'Вход успешен')
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Что-то пошло не так. Проверьте введённую информацию.')
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Войдите, что-бы продолжить')
+    return redirect('login')
