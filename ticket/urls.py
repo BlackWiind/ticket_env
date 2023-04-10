d327ad8d629ed48da49c18c4411cb807fcf7ea45
Акакиев Акакий <BlackWiind@gmail.com>
2023-04-10 14:00:17 +1000

    Некоторые функции представлений переделены в классы, добавлина сортировка таблиц по колонкам

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
+from .views import Ticket_Details, All_Tickets, Ticket_queue, Workspace, All_Closed_Tickets
 
 urlpatterns = [
-    path('ticket-details/<int:pk>/', views.ticket_details, name='ticket-details'),
+    path('ticket-details/<int:pk>/', Ticket_Details.as_view(), name='ticket-details'),
     path('create-ticket/', views.create_ticket, name='create-ticket'),
     path('update-ticket/<int:pk>/', views.update_ticket, name='update-ticket'),
-    path('all-tickets/', views.all_tickets, name='all-tickets'),
-    path('ticket-queue/', views.ticket_queue, name='ticket-queue'),
+    path('all-tickets/', All_Tickets.as_view(), name='all-tickets'),
+    path('ticket-queue/', Ticket_queue.as_view(), name='ticket-queue'),
     path('accept-ticket/<int:pk>/', views.accept_ticket, name='accept-ticket'),
     path('close-ticket/<int:pk>/', views.close_ticket, name='close-ticket'),
-    path('workspace/', views.workspace, name='workspace'),
-    path('all-closed-tickets/', views.all_closed_tickets, name='all-closed-tickets'),
+    path('workspace/', Workspace.as_view(), name='workspace'),
+    path('all-closed-tickets/', All_Closed_Tickets.as_view(), name='all-closed-tickets'),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,20 +1,41 @@
 import datetime
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.generic import DetailView
+
 from .models import Ticket
 from .form import CreateTicketForm, UpdateTicketForm
 from users.models import User
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
 
 
 # детали заявки
-@login_required
-def ticket_details(request, pk):
-    ticket = Ticket.objects.get(pk=pk)
-    users_tickets = User.objects.get(username=ticket.created_by)
-    tickets_legacy = users_tickets.created_by.all()
-    context = {'ticket': ticket, 'tickets_legacy': tickets_legacy}
-    return render(request, 'ticket/ticket_details.html', context)
+class Ticket_Details(DetailView):
+    model = Ticket
+    template_name = 'ticket/ticket_details.html'
+
+
+class Tickets_Legacy(ListView):
+    paginate_by = 5
+    model = Ticket
+    template_name = 'ticket/all_tickets.html'
+    context_object_name = 'tickets'
+
+    def get_queryset(self):
+        return Ticket.objects.filter(created_by=self.request.user).order_by('-date_created')
+
+
+
+# @login_required
+# def ticket_details(request, pk):
+#     ticket = Ticket.objects.get(pk=pk)
+#     users_tickets = User.objects.get(username=ticket.created_by)
+#     tickets_legacy = users_tickets.created_by.all()
+#     context = {'ticket': ticket, 'tickets_legacy': tickets_legacy}
+#     return render(request, 'ticket/ticket_details.html', context)
 
 
 """Для юзеров"""
@@ -64,22 +85,48 @@ def update_ticket(request, pk):
 
 
 # Просмотр всех созданных заявок
-@login_required
-def all_tickets(request):
-    tickets = Ticket.objects.filter(created_by=request.user).order_by('-date_created')
-    context = {'tickets': tickets}
-    return render(request, 'ticket/all_tickets.html', context)
+class All_Tickets(ListView):
+    paginate_by = 5
+    model = Ticket
+    template_name = 'ticket/all_tickets.html'
+    context_object_name = 'tickets'
+
+    def get_queryset(self):
+        return Ticket.objects.filter(created_by=self.request.user).order_by('-date_created')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+#
+# @login_required
+# def all_tickets(request):
+#     tickets = Ticket.objects.filter(created_by=request.user).order_by('-date_created')
+#     paginator = Paginator(tickets, 10)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {'tickets': tickets, 'page_obj': page_obj}
+#     return render(request, 'ticket/all_tickets.html', context)
 
 
 """Для сотрудников"""
 
 
 # просмотр очетеди заявок
-@login_required
-def ticket_queue(request):
-    tickets = Ticket.objects.filter(ticket_status='Pending')
-    context = {'tickets': tickets}
-    return render(request, 'ticket/ticket_queue.html', context)
+class Ticket_queue(ListView):
+    model = Ticket
+    paginate_by = 5
+    template_name = 'ticket/ticket_queue.html'
+    context_object_name = 'tickets'
+
+    def get_queryset(self):
+        return Ticket.objects.filter(ticket_status='Pending')
+
+# @login_required
+# def ticket_queue(request):
+#     tickets = Ticket.objects.filter(ticket_status='Pending')
+#     context = {'tickets': tickets}
+#     return render(request, 'ticket/ticket_queue.html', context)
 
 
 # Принтяие заявки из очереди
@@ -106,16 +153,36 @@ def close_ticket(request, pk):
 
 
 # Заявка в работе
-@login_required
-def workspace(request):
-    tickets = Ticket.objects.filter(assigned_to=request.user, is_resolved=False)
-    context = {'tickets': tickets}
-    return render(request, 'ticket/workspace.html', context)
+class Workspace(ListView):
+    model = Ticket
+    paginate_by = 5
+    template_name = 'ticket/workspace.html'
+    context_object_name = 'tickets'
+
+    def get_queryset(self):
+        return Ticket.objects.filter(assigned_to=self.request.user, is_resolved=False)
+
+
+# @login_required
+# def workspace(request):
+#     tickets = Ticket.objects.filter(assigned_to=request.user, is_resolved=False)
+#     context = {'tickets': tickets}
+#     return render(request, 'ticket/workspace.html', context)
 
 
 # Все закрытые заявки
-@login_required
-def all_closed_tickets(request):
-    tickets = Ticket.objects.filter(assigned_to=request.user, is_resolved=True)
-    context = {'tickets': tickets}
-    return render(request, 'ticket/all_closed_tickets.html', context)
+class All_Closed_Tickets(ListView):
+    model = Ticket
+    paginate_by = 5
+    template_name = 'ticket/all_closed_tickets.html'
+    context_object_name = 'tickets'
+
+    def get_queryset(self):
+        return Ticket.objects.filter(assigned_to=self.request.user, is_resolved=True)
+
+
+# @login_required
+# def all_closed_tickets(request):
+#     tickets = Ticket.objects.filter(assigned_to=request.user, is_resolved=True)
+#     context = {'tickets': tickets}
+#     return render(request, 'ticket/all_closed_tickets.html', context)
